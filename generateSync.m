caa2e9fcfafb0a4fa2651ef2c22ef7d53706222e
Lubomir Jagos <lubomir.jagos@gmail.com>
2017-05-12 03:18:50 +0200

    Funkcie na generovanie sync slov funguju, pulzy musia byt v mojom pripade na kladnych indexoch, lebo DC = fftlen/2+1 takze pre 128 je DC = 129 a potom su hodnoty na 130, 132, 134, ...
    420B signal z Gnuradia sa nezhoduje s generovanym!

--- generateSync.m
+++ generateSync.m
@@ -1,19 +1,24 @@
-function sync = generateSync1(ifftLen,occupiedCarriers,pilotCarriers)
-    carriers = [];
-    for k = 1:length(occupiedCarriers)
-        if (rem(occupiedCarriers(k),2) == 0)
-            carriers = [carriers occupiedCarriers(k)];
-        end
-    end
-    for k = 1:length(pilotCarriers)
-        if (rem(pilotCarriers(k),2) == 0)
-            carriers = [carriers pilotCarriers(k)];
-        end
-    end
-    carriers = sort([carriers]);
-    seq = randsrc(1,length(carriers));
+function sync = generateSync(ifftLen,occupiedCarriers,pilotCarriers)
+    sync1 = generateSync1(ifftLen, occupiedCarriers, pilotCarriers);
+    dlmwrite('sync1.txt',sync1,',');
+    sync2 = generateSync2(ifftLen, occupiedCarriers, pilotCarriers);
+    dlmwrite('sync2.txt',sync2,',');
+
+    f = fopen('sync1.txt','r');
+    content1 = fread(f, inf,'uint8')';
+    fclose(f);
+    f = fopen('sync1.txt','w');
+    fwrite(f,horzcat('[',content1(1:end-1),']'));
+    fclose(f);
+
+    f = fopen('sync2.txt','r');
+    content2 = fread(f, inf,'uint8')';
+    fclose(f);
+    f = fopen('sync2.txt','w');
+    fwrite(f,horzcat('[',content2(1:end-1),']'));
+    fclose(f);
     
-    sync = zeros(1,ifftLen);
-    sync(carriers) = seq*sqrt(2);
-    sync(1) = 0+0i;
+    % for gnuradio
+    
+    
 end
--- generateSync1.m
+++ generateSync1.m
@@ -1,12 +1,15 @@
 function sync = generateSync1(ifftLen,occupiedCarriers,pilotCarriers)
     carriers = [];
     for k = 1:length(occupiedCarriers)
-        if (rem(occupiedCarriers(k),2) == 1)
+        if (occupiedCarriers(k) < 0)
+            occupiedCarriers(k) = occupiedCarriers(k)+ifftLen+1;
+        end
+        if (rem(occupiedCarriers(k),2) == 0)
             carriers = [carriers occupiedCarriers(k)];
         end
     end
     for k = 1:length(pilotCarriers)
-        if (rem(pilotCarriers(k),2) == 1)
+        if (rem(pilotCarriers(k),2) == 0)
             carriers = [carriers pilotCarriers(k)];
         end
     end
